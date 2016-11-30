37fa931226da778cfa226b047e82b91d1edb9ba9
Tina Liang <tina.liang1@duke.edu>
2016-11-30 06:12:55 +0000

    changed home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # when app first starts, goes to this controller first from routes.db so set home page here
   protect_from_forgery with: :exception
+  
+  def hello
+    render html:"hello world!"
+  end
+  
 end
